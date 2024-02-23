78a018bb1583b5ec4e3a261561acf625520ac010
Babatunde <sa.babatunde@yahoo.com>
2024-02-23 11:47:30 +0000

    ../.viminfo

--- tesla/deploy.sh
+++ tesla/deploy.sh
@@ -3,6 +3,10 @@ Deploy python apps
 deploy the dev env
 deployment5 master
 deployment4
+<<<<<<< HEAD
 deploy the application to staging env
 echo good job.
 Tested and accepted
+=======
+deploy the application to changes
+>>>>>>> 0ed6c15 (deployment to the staging environment)
